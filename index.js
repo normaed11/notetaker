375792739e11011b8c4970132c61e28de00ea18d
normadiaz <normadiazchb@me.com>
2022-03-12 12:02:34 -0600

    add end point get and post

--- index.js
+++ index.js
@@ -1,15 +1,35 @@
 // imports express
+const { text } = require('express')
 const express = require('express')
 const { sendfile } = require('express/lib/response')
+// imports Body Parser
+const bodyParser = require('body-parser')
+
 // allows user to user methods: get post update put delete path
 const app = express()
+// middlewear to iniciate bodyparser
+app.use(bodyParser.json(), bodyParser.urlencoded({ extend: false }))
 // middlewear helps grap static files
-app.use(express.static(__dirname + '/miniature-eureka/Develop/public/assets/css'))
+app.use(express.static(__dirname + '/miniature-eureka/Develop/public/assets'))
 // http request through get method 
 app.get('/', function (req, res) {
     res.sendFile(__dirname + '/miniature-eureka/Develop/public/index.html')
 })
 app.get('/notes/', function (req, res) {
     res.sendFile(__dirname + '/miniature-eureka/Develop/public/notes.html')
+})
+app.get('/api/notes', function (req, res) {
+    res.send([{
+        title: 'prueba',
+        text: 'estoy haciendo una prueba de texto'
+    },
+    {
+        title: 'segunda',
+        text: 'segundo chequeo'
+    }])
+
+})
+app.post('/api/notes', function (req, res) {
+    console.log(req.body)
 })
 app.listen(8080)
