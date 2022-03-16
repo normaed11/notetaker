6a471a3fe55d15fcef0ab4a7c4db4e0bb3ad10cf
normadiaz <normadiazchb@me.com>
2022-03-15 19:13:20 -0500

    end point save note and to add not

--- index.js
+++ index.js
@@ -2,8 +2,18 @@
 const { text } = require('express')
 const express = require('express')
 const { sendfile } = require('express/lib/response')
+const fs = require('fs');
+let notes = require('./miniature-eureka/Develop/db/db.json')
 // imports Body Parser
 const bodyParser = require('body-parser')
+// var notes = [{
+//     title: 'prueba',
+//     text: 'estoy haciendo una prueba de texto'
+// },
+// {
+//     title: 'segunda',
+//     text: 'segundo chequeo'
+// }]
 
 // allows user to user methods: get post update put delete path
 const app = express()
@@ -13,23 +23,30 @@ app.use(bodyParser.json(), bodyParser.urlencoded({ extend: false }))
 app.use(express.static(__dirname + '/miniature-eureka/Develop/public/assets'))
 // http request through get method 
 app.get('/', function (req, res) {
+    console.log(notes)
     res.sendFile(__dirname + '/miniature-eureka/Develop/public/index.html')
 })
 app.get('/notes/', function (req, res) {
     res.sendFile(__dirname + '/miniature-eureka/Develop/public/notes.html')
 })
 app.get('/api/notes', function (req, res) {
-    res.send([{
-        title: 'prueba',
-        text: 'estoy haciendo una prueba de texto'
-    },
-    {
-        title: 'segunda',
-        text: 'segundo chequeo'
-    }])
+    res.send(notes)
 
 })
 app.post('/api/notes', function (req, res) {
+    // console.log(req.body)
+    notes.push(req.body)
+    let data = JSON.stringify(notes);
+
+    fs.writeFileSync('./miniature-eureka/Develop/db/db.json', data);
+
+})
+app.delete('/api/notes', function (req, res) {
     console.log(req.body)
+    // notes.push(req.body)
+    // let data = JSON.stringify(notes);
+
+    // fs.writeFileSync('./miniature-eureka/Develop/db/db.json', data);
+
 })
 app.listen(8080)
